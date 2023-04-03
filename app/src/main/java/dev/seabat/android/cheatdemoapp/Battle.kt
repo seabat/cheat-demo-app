b67dfa3c22ea372ca63a3c7dd922313d16563ecc
seabat <ryouta.nakanishi.86@gmail.com>
2023-04-04 02:25:21 +0900

    戦闘の仮実装

---
+++ app/src/main/java/dev/seabat/android/cheatdemoapp/Battle.kt
@@ -0,0 +1,10 @@
+package dev.seabat.android.cheatdemoapp
+
+data class Battle (
+    val braveHp: Int,
+    val braveAttack: Int,
+    val braveDefence: Int,
+    val enemyHp: Int,
+    val enemyAttack: Int,
+    val enemyDefence: Int
+    )
---
+++ app/src/main/java/dev/seabat/android/cheatdemoapp/Constants.kt
@@ -0,0 +1,10 @@
+package dev.seabat.android.cheatdemoapp
+
+object Constants {
+    val BRAVE_HP: Int = 70
+    val BRAVE_ATTACK = 30
+    val BRAVE_DEFENCE = 20
+    val SATAN_HP: Int = 70
+    val SATAN_ATTACK = 30
+    val SATAN_DEFENCE = 20
+}
